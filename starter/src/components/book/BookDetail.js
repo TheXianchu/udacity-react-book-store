64665087021c6de19ec60e6f6dccb3b089ef32fe
Darryll de Quillettes <darryll@interactivestudios.nl>
2022-07-14 11:56:59 +0200

    feat: add detail page

--- starter/src/components/book/BookDetail.js
+++ starter/src/components/book/BookDetail.js
@@ -1,17 +1,35 @@
 import BookCover from "./BookCover";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function BookDetail() {
   const location = useLocation();
   const { book } = location.state;
 
   return book ? (
-    <div className="book">
-      <div className="book-top">
-        {book.imageLinks ? <BookCover imageLinks={book.imageLinks} /> : null}
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors}</div>
+      <div className="centered">
+        <div className="list-books-content">
+          <div className="book">
+            <div className="book-top">
+              {book.imageLinks ? (
+                <BookCover imageLinks={book.imageLinks} />
+              ) : null}
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+          </div>
+        </div>
+        <h3>Rating: {book.averageRating}</h3>
+
+        <div className="line-break">{book.description}</div>
+      </div>
+
+      <div className="return-detail">
+        <Link to="/">Close</Link>
+      </div>
     </div>
   ) : null;
 }
